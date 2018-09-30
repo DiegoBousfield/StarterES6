2229cdb353fe6fafbefcc9c347f2ef388af54075
Diego Bousfield <diegobousfield@gmail.com>
2018-09-30 15:29:06 -0300

    Add config for mod3 exercises

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import ClasseUsuario, { idade as IdadeUsuario} from './mod2/function';
+import { umPorSegundo } from './mod3/exerc1';
 
-  ClasseUsuario.info();
-  console.log(IdadeUsuario); 
+umPorSegundo();
+/*  ClasseUsuario.info();
+  console.log(IdadeUsuario); */
