8dc19c87fecb40c08a5c0219eba137bf68988511
Diego Bousfield <diegobousfield@gmail.com>
2018-10-01 10:18:38 -0300

    Add mod3 exercise 3

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import ClasseUsuario, { idade as IdadeUsuario} from './mod2/function';
 import { umPorSegundo } from './mod3/exerc1';
 import { getUserFromGithub } from './mod3/exerc2';
+import Github from './mod3/exerc3';
 
-umPorSegundo();
+/*umPorSegundo();
 getUserFromGithub('DiegoBousfield');
 getUserFromGithub('DiegoBousfield2');
-ClasseUsuario.info();
+ClasseUsuario.info();*/
+Github.getRepositories('DiegoBousfield/learningwebpack');
+Github.getRepositories('rocketseat/asldçkfjads');
 console.log(IdadeUsuario);
